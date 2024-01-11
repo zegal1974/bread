94e20c6c1a209f32d9d189ce2c93b836044a6aa9
zegal <18908302251@189.cn>
2024-01-11 17:27:18 +0800

    add react-qurey

---
+++ client/src/components/Error.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Loading extends React.Component {
+  render() {
+    return (
+      <>
+        <div>
+          Error
+        </div>
+      </>
+    );
+  }
+}
+
+export default Loading;
---
+++ client/src/services.js
@@ -0,0 +1,9 @@
+
+
+function getActors(start = 0, page = 1){
+  return []
+}
+
+
+
+export default { getActors }
